5b5b2a5f27c13a1328ebf54ca5fe116743fa0c78
程馨兰 <Couvrir@hotmail.com>
2020-12-21 10:56:06 +0800

    Delete main.java

--- checkPasswd/src/main/java/com/couvrir/checkpassword/main.java
+++
@@ -1,8 +0,0 @@
-package com.couvrir.checkpassword;
-
-public class main {
-    public static void main(String[] args)  {
-        String passwd = "2hAj5#mne-ix.86H";
-        System.out.println(CheckPassword.checkPasswordInDic(passwd));
-    }
-}
